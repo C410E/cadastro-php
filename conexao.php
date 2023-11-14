264792430fec2f8f5a19b84f32a6265b6ca48de3
caioe <caioevandro0@gmail.com>
2023-11-14 16:07:10 -0300

    erro em listar alunos, resolver depois

--- conexao.php
+++ conexao.php
@@ -1,13 +1,12 @@
 <?php
 try {
     $conexao = new PDO('pgsql:localhost:3000;dbname=cadastro_php', 'postgresql', '654789123');
-} catch (Exception $error) {
+} catch (Exception $erro) {
     try {
     $conexao = new PDO('pgsql:host=www.db4free.net;dbname=alunos_rogado', 'arthurrogado', 'arthurrogado');
     } catch (Exception $e) {
-        echo $erro -> getMessage();
-        echo "<br>";
-        echo $erro -> getCode();
+        $erro = $e->getMessage();
+        echo "Erro: $erro";
     }
 }
 ?>
